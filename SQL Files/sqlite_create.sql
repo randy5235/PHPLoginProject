a563251d0abcb1df132ecc4c21c34ccab6470769
randy5235 <randy5235@hotmail.com>
2015-02-26 18:19:13 -0800

    Added some comments to the sqlite_create.sql file.

--- SQL Files/sqlite_create.sql
+++ SQL Files/sqlite_create.sql
@@ -1,3 +1,7 @@
+# use this file to create the default sqlite file for testing and mobile use.
+# Written by Randy Wressell 2/2015
+
+
 create table USER(
     Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     FirstName nvarchar(25) NOT NULL,
