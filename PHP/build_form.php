b666e8df6a925b2ae730d10bfb2e29b9532d8c67
randy5235 <randy5235@hotmail.com>
2015-03-04 17:49:13 -0800

    Added a stub for the build_form.php file to be used to securely generate the form ( field names will be random and it will add a honeypot field as well that is never meant to have a value as it will be hidden from the user)

---
+++ PHP/build_form.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Randy
+ * Date: 3/4/15
+ * Time: 5:46 PM
+ */
+//  hash("sha256",uniqid("", true)); use the to generate the pseudo random form field names (is this randp, enough?) since the field names only need to be random not secure cryptographically
